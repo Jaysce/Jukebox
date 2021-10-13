fbe43e34576c236b8132ae2ca4d42b7cf0cb79ec
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2021-10-13 14:29:59 +1300

    Initial Commit

---
+++ Jukebox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jukebox
+//
+//  Created by Sasindu Jayasinghe on 13/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jukebox/JukeboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  JukeboxApp.swift
+//  Jukebox
+//
+//  Created by Sasindu Jayasinghe on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct JukeboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
