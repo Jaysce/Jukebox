e467aee5e3717b77a2a1695c80dd5c9a2337dc66
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2021-10-13 15:36:34 +1300

    Add model for Track

---
+++ Jukebox/Models/Track.swift
@@ -0,0 +1,18 @@
+//
+//  Track.swift
+//  Jukebox
+//
+//  Created by Sasindu Jayasinghe on 13/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Track {
+    
+    let title: String
+    let artist: String
+    let albumArt: NSImage
+    let lyrics: String
+    
+}
