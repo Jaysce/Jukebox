65611bce821cd5335c856cfe77f4c42f9cdea394
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2021-10-29 23:50:23 +1300

    Add temporary template App Icon and update PreferencesView

--- Jukebox/Views/PreferencesView.swift
+++ Jukebox/Views/PreferencesView.swift
@@ -10,33 +10,15 @@ import SwiftUI
 struct PreferencesView: View {
     
     weak var parentWindow: PreferencesWindow!
-    @State private var isHoveringCloseButton = false
     
     var body: some View {
         HStack(spacing: 0) {
             ZStack {
                 VisualEffectView(material: .sidebar, blendingMode: .behindWindow)
-                VStack {
-                    HStack {
-                        Button {
-                            parentWindow.close()
-                            isHoveringCloseButton = false
-                        } label: {
-                            Image(isHoveringCloseButton ? "close_hover" : "close")
-                        }
-                        .pressButtonStyle()
-                        .onHover(perform: { hovering in
-                            if hovering { isHoveringCloseButton = true }
-                            else { isHoveringCloseButton = false }
-                        })
-                        .padding(12)
-                        Spacer()
-                    }
-                    
-                    Spacer()
-                }
+                CloseButton(parentWindow: parentWindow)
+                AppInfo().offset(x: 1) // Looks like it was off center (?)
             }
-            .frame(maxWidth: 120, maxHeight: .infinity)
+            .frame(maxWidth: 140, maxHeight: .infinity)
             
             Divider()
             
@@ -47,5 +29,67 @@ struct PreferencesView: View {
         }
         .ignoresSafeArea()
     }
+}
+
+struct CloseButton: View {
     
+    weak var parentWindow: PreferencesWindow!
+    @State private var isHoveringCloseButton = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    parentWindow.close()
+                    isHoveringCloseButton = false
+                } label: {
+                    Image(isHoveringCloseButton ? "close_hover" : "close")
+                }
+                .pressButtonStyle()
+                .onHover(perform: { hovering in
+                    if hovering { isHoveringCloseButton = true }
+                    else { isHoveringCloseButton = false }
+                })
+                .padding(12)
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct AppInfo: View {
+    
+    let jukeboxRepo = "https://github.com/Jaysce/Jukebox"
+    let jukeboxWebPage = "https://jaysce.dev"
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(nsImage: NSImage(named: "AppIcon") ?? NSImage())
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            HStack {
+                Button {
+                    NSWorkspace.shared.open(URL(string: jukeboxRepo)!)
+                } label: {
+                    Text("GitHub").font(.system(size: 12))
+                }
+                .buttonStyle(LinkButtonStyle())
+                
+                Button {
+                    NSWorkspace.shared.open(URL(string: jukeboxWebPage)!)
+                } label: {
+                    Text("Website").font(.system(size: 12))
+                }
+                .buttonStyle(LinkButtonStyle())
+            }
+        }
+    }
+}
+
+struct PreferencesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferencesView()
+    }
 }
